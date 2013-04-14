21ff64e05880c45253ccaa140b3379be91996a0e
Claes Nygren <slaskhas@gmail.com>
2013-04-13 22:36:42 -0700

    Node uuid

--- routes/user.js
+++ routes/user.js
@@ -6,7 +6,6 @@ var azure = require('azure');
 var uuid = require('node-uuid');
 
 exports.list = function(req, res){
-  var client = azure.createTableService();
 
 
   res.send("respond with a resource");
